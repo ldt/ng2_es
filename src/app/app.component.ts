fcc6ba39e0df4f1e8f6cc88dd6205762e722a66b
Laurent Dosdat <laurent.dosdat@calopea.com>
2016-10-06 10:31:46 +0200

    commented elasticsearch import added jquery and test if it worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import * as elasticsearch from 'elasticsearch';
+// import * as elasticsearch from 'elasticsearch';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
-  private _client: elasticsearch.ClientInterface
+  // private _client: elasticsearch.ClientInterface
 
   constructor() {
-    this._client = elasticsearch.Client();
+    // this._client = elasticsearch.Client();
+  }
+
+  ngOnInit() {
+    $('h1').text('jquery works');
   }
 }
